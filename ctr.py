89b9929e6df46368d3ce1bd34b6832124f3652e1
Anmol1696 <anmol1696@gmail.com>
2017-06-14 22:03:51 +0530

    Adding plain text xor

--- ctr.py
+++ ctr.py
@@ -5,8 +5,9 @@
 from src.main import main_encrypt, main_decrypt
 
 import time
+import math
 
-def main_ctr(number_of_output, key, nonce, counter=0):
+def main_ctr(number_of_output, key, iv, counter=0):
     """
         Define IV or Nonce, of 16 hexadecimal, rest will be counter
         number of output is a var that gives the number of times we have to repeate
@@ -16,7 +17,7 @@ def main_ctr(number_of_output, key, nonce, counter=0):
     plain_input = ''
 
     for _ in xrange(number_of_output):
-        inp             = nonce + hex(counter)[2:].zfill(16)
+        inp             = iv + hex(counter)[2:].zfill(16)
         plain_input    += inp 
         counter        += 1
     cipher = main_encrypt(plain_input, key)
@@ -24,13 +25,30 @@ def main_ctr(number_of_output, key, nonce, counter=0):
     
     return output
 
+def plain_text_xor_ctr_output(plain_text, key, iv):
+    """
+        XORing plain text with CTR
+        Here 32 is the block size corresponding to 16 bytes
+        Plain text should be in one single string
+    """
+    plain_blocks = [plain_text[i:i+32] for i in range(0, len(plain_text), 32)]
+    plain_blocks[-1] = plain_blocks[-1] + '0'*(32 - len(plain_blocks[-1]))
+
+    count = len(plain_blocks)
+
+    output = main_ctr(count, key, iv)
+
+    final_output = [hex(int(ouput[i], 16) ^ int(plain_blocks[i],16))[2:34] for i in range(count)]
+
+    return final_output
+
 if __name__ == "__main__":
     key = '328831e0435a3137f6309807a88da234'
     count = 10
-    nonce = '2b28ab097eaef7cf'
+    iv = '2b28ab097eaef7cf'
 
     start = time.time()
-    output = main_ctr(count, key, nonce)
+    output = main_ctr(count, key, iv)
     stop = time.time()
     print output
     print 'Done'
